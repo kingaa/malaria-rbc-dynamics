18b13a75cab122026c1672482c2b38be4d852e16
Madeline <madelineapeters@gmail.com>
2023-10-19 16:51:25 -0400

    Update POMP_GroupLevel_ModelFitting_ER.R

--- final_files/Manuscript/POMP_GroupLevel_ModelFitting_ER.R
+++ final_files/Manuscript/POMP_GroupLevel_ModelFitting_ER.R
@@ -7,6 +7,9 @@ library(iterators)
 library(doFuture)
 plan(multisession)
 
+seed_choice <- 851657743
+set.seed(seed_choice)
+
 #### Make data frame with all of the data ####
 
 #Load in PNAS trajectories
@@ -34,7 +37,7 @@ sample_size <- 1000
 foreach (
   id=mouse_id_list,
   .combine=rbind,
-  .options.future = list(seed = 851657743)
+  .options.future = list(seed = seed_choice)
 ) %dofuture% {
   
   key_lik_df <- sm1_mod |> filter(mouseid==id) |> select(key,lik) |> unique()
@@ -100,9 +103,9 @@ foreach (bp=iter(breakpoint_grid,"row"),
   
   #Models
   models <- list(
-    m1=lm(R~poly(lagE,2):(box-1):(phase-1)+(box-1):(phase-1),data=df),
-    m2=lm(R~(poly(lagE,2)+box-1):(phase-1)+(phase-1),data=df),
-    m3=lm(R~poly(lagE,2):(phase-1)+(phase-1),data=df),
+    m1=lm(R~poly(lagE,2,raw=T):(box-1):(phase-1)+(box-1):(phase-1),data=df),
+    m2=lm(R~(poly(lagE,2,raw=T)+box-1):(phase-1)+(phase-1),data=df),
+    m3=lm(R~poly(lagE,2,raw=T):(phase-1)+(phase-1),data=df),
     m4=lm(R~lagE:(box-1):(phase-1)+(box-1):(phase-1),data=df),
     m5=lm(R~(lagE+box-1):(phase-1)+(phase-1),data=df),
     m6=lm(R~lagE:(phase-1)+(phase-1),data=df)
@@ -122,14 +125,148 @@ joint_repped_df |>
   ) -> df
 
 models <- list(
-  m1=lm(R~poly(lagE,2):(box-1):(phase-1)+(box-1):(phase-1),data=df),
-  m2=lm(R~(poly(lagE,2)+box-1):(phase-1)+(phase-1),data=df),
-  m3=lm(R~poly(lagE,2):(phase-1)+(phase-1),data=df),
+  m1=lm(R~poly(lagE,2,raw=T):(box-1):(phase-1)+(box-1):(phase-1),data=df),
+  m2=lm(R~(poly(lagE,2,raw=T)+box-1):(phase-1)+(phase-1),data=df),
+  m3=lm(R~poly(lagE,2,raw=T):(phase-1)+(phase-1),data=df),
   m4=lm(R~lagE:(box-1):(phase-1)+(box-1):(phase-1),data=df),
   m5=lm(R~(lagE+box-1):(phase-1)+(phase-1),data=df),
   m6=lm(R~lagE:(phase-1)+(phase-1),data=df)
 )
 
 chosen_model <- models[which(paste0("m",1:6)==joint_AIC_df[which(joint_AIC_df$AIC==min(joint_AIC_df$AIC)),1])]
-chosen_model
+chosen_model[[1]]
 
+library(jtools)
+chosen_model_summ <- summ(chosen_model[[1]],confint=TRUE)
+chosen_model_coeftab <- chosen_model_summ$coeftable |> 
+  as.data.frame() |> 
+  rownames_to_column()
+
+bake(file="jnd.rds",{
+
+  joint <- data.frame()  
+  
+  for (box_choice in box_list){
+   
+    df_box <- df |> filter(box==box_choice) #filter data for chosen box
+    coeftab_box <- chosen_model_coeftab |> 
+      filter(grepl(box_choice,rowname)) |>
+      select(rowname,`2.5%`,`97.5%`) #create table with coefficients for chosen box
+    
+    coeftab_box$rowname <- lapply(1:nrow(coeftab_box),FUN=function(r){
+      
+      if(grepl("raw",name, fixed=TRUE)){
+        
+        x <- strsplit(coeftab_box$rowname[r],", raw = T")[[1]] |> cat(sep="") |> capture.output()
+        y <- strsplit(x,paste0("box",box_choice,":"))[[1]]
+        z <- capture.output(cat(y[1:length(y)]))
+        output <- gsub(" phase", "phase", z)
+        
+      } else {
+          
+        x <- strsplit(coeftab_box$rowname[r],paste0("box",box_choice,":"))[[1]]
+        y <- capture.output(cat(x[1:length(x)]))
+        output <- gsub(" phase", "phase", y)
+        
+      }
+      output
+    }) #rewrite rowname column to match coefficient names from lm
+    
+    #Create lm object whose coefficients will be manipulated
+    m01 <- lm(R~poly(lagE,2):(phase-1)+(phase-1),data=df_box)
+    
+    foreach (r=1:1000,
+             .combine=rbind,
+             .options.future = list(seed = seed_choice)
+    ) %dofuture% { #for loop over reps
+      
+      m02 <- m01 
+      
+      for (i in seq(length(coef(m02)))){ #for loop over coefficients
+        
+        coef_name <- names(coef(m02)[i])  
+        
+        coeftab_box_sub <- coeftab_box |> filter(rowname==coef_name)
+        a <- coeftab_box_sub$`2.5%`
+        b <- coeftab_box_sub$`97.5%`
+        
+        samp <- runif(1,a,b)
+        
+        m02$coefficients[i] <- samp
+        
+      } #end of for statement over i
+      
+      df_box_phase1 <- df_box |> filter(phase==1)
+      df_box_phase2 <- df_box |> filter(phase==2)
+      
+      newdata_phase1 <- data.frame(lagE=seq(min(df_box_phase1$lagE), max(df_box_phase1$lagE), 1000))
+      newdata_phase1$phase <- 1
+      newdata_phase2 <- data.frame(lagE=seq(min(df_box_phase2$lagE), max(df_box_phase2$lagE), 1000))
+      newdata_phase2$phase <- 2
+      
+      newdata <- rbind(newdata_phase1,newdata_phase2)
+      newdata$phase <- factor(newdata$phase)
+      
+      pred1 <- sapply(filter(newdata,phase==1)$lagE,FUN=function(x){
+        m02$coefficients[1]+
+          m02$coefficients[3]*x+
+          m02$coefficients[4]*x^2
+      })
+      
+      pred2 <- sapply(filter(newdata,phase==2)$lagE,FUN=function(x){
+        m02$coefficients[2]+
+          m02$coefficients[5]*x+
+          m02$coefficients[6]*x^2
+      })
+      
+      newdata$pred <- c(pred1,pred2)
+      newdata$rep <- r
+      newdata$box <- box_choice
+      
+      newdata
+      
+    } -> box_newdata
+    
+    joint <- rbind(joint,box_newdata)
+     
+  } #end of for statement over boxes
+    
+    joint
+  
+}) -> joint_newdata #end of baking
+
+joint_newdata$pABA <- factor(joint_newdata$box,
+                                levels=c("box04","box03","box02","box01"),
+                                labels=c("0%","0.0005%","0.005%","0.05%"))
+
+joint_newdata |>
+  group_by(pABA,lagE,phase) |>
+  summarise(
+    min=min(pred),
+    avg=mean(pred),
+    max=max(pred)
+  ) -> joint_group
+  
+(pABAphases <- ggplot()+
+  geom_line(data=filter(joint_group, phase==1),aes(x=lagE,y=avg,col=pABA),linewidth=2)+
+  geom_line(data=filter(joint_group, phase==2),aes(x=lagE,y=avg,col=pABA),linewidth=2)+
+  geom_ribbon(data=filter(joint_group, phase==1),aes(x=lagE,ymin=min,ymax=max,fill=pABA),alpha=0.2)+
+  geom_ribbon(data=filter(joint_group, phase==2),aes(x=lagE,ymin=min,ymax=max,fill=pABA),alpha=0.2)+
+  xlab("Erythrocytes (t-1)")+ylab("Predicted reticulocyte supply (t)")+
+  scale_colour_manual(values=cbPalette[2:5])+
+  scale_fill_manual(values=cbPalette[2:5])+
+  theme_bw()+
+  theme(
+    axis.title=element_text(size=15),
+    axis.text=element_text(size=13),
+    panel.grid=element_blank(),
+    legend.position=c(0.8,0.7),
+    legend.title=element_text(size=15),
+    legend.text=element_text(size=13)
+  )
+)
+  
+ggsave("pABAphases.png",plot=pABAphases,
+       width=20,height=15,units="cm") 
+
+#Careful when doing the prediction, do the lines start where you think they should start and not at zero
