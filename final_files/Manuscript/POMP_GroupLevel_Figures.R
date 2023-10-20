88f4600bd579b28ef4e4f129a79644f98314d50e
Madeline <madelineapeters@gmail.com>
2023-10-20 14:54:13 -0400

    Update POMP_GroupLevel_Figures.R

--- final_files/Manuscript/POMP_GroupLevel_Figures.R
+++ final_files/Manuscript/POMP_GroupLevel_Figures.R
@@ -12,6 +12,10 @@ cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
 
 source("POMP_GroupLevel_DataPrep.R")
 
+#############################################################################
+#### Data figures ####
+#############################################################################
+
 (reticVStime <- group_traj |>
   filter(variable=="R",box!="05",time<20) |>
   ggplot()+
@@ -106,4 +110,49 @@ ggsave("pABA0swirl.png",plot=pABA0swirl,
 )
 ggsave("pABAswirl.png",plot=pABAswirl,
        width=17,height=15,units="cm")
-  
+
+#############################################################################
+#### Model relationship figures ####
+#############################################################################
+group_traj |>
+  filter(pABA=="0%",variable%in%c("E","R","N")) |>
+  select(-lo,-hi) |>
+  pivot_wider(names_from="variable",values_from="med") -> fig_df
+
+Erange <- c(0,1)
+expFun <- function(E){1/(1+exp(-10*(-E+0.5)))}
+
+ggplot()+
+  geom_line(aes(x=Erange,y=c(0.25,0.25)),col=cbPalette[6],linewidth=2)+
+  geom_text(aes(x=Erange[1],y=0.29,label="Constant supply (a)"),col=cbPalette[6],hjust=0)+
+  geom_line(aes(x=Erange,y=c(1,0)),col=cbPalette[4],linewidth=2)+
+  geom_text(aes(x=Erange[1]+0.25,y=0.8,label="Linear supply (b)"),col=cbPalette[4],hjust=0,angle=-41)+
+  geom_line(aes(x=seq(0,1,0.01),y=sapply(seq(0,1,0.01),expFun)),linewidth=2,col=cbPalette[5])+
+  geom_text(aes(x=Erange[1]+0.2,y=0.99,label="Hill function supply (c)"),col=cbPalette[5],hjust=0)+
+  ylim(0,1)+
+  xlab("Erythrocytes (t-1)")+ylab("Reticulocyte supply (t)")+
+  theme_bw()+
+  theme(axis.title=element_text(size=15),
+        axis.text=element_blank(),
+        axis.ticks=element_blank(),
+        panel.grid=element_blank())
+
+ggplot()+
+  geom_smooth(aes(x=fig_df$time,y=fig_df$N),se=FALSE,col=cbPalette[2],linewidth=2)+
+  geom_text(aes(x=21,y=quantile(fig_df$N,probs=0.85),label="Time-varying killing (b)"),col=cbPalette[2])+
+  geom_line(aes(x=c(0,30),y=quantile(fig_df$N,probs=0.25)),col=cbPalette[1],linewidth=2)+
+  geom_text(aes(x=15,y=quantile(fig_df$N,probs=0.35),label="Constant killing (a)"),col=cbPalette[1])+
+  geom_path(aes(x=c(0,7,7,23,23,30),y=c(
+    quantile(fig_df$N,probs=0.5),
+    quantile(fig_df$N,probs=0.5),
+    quantile(fig_df$N,probs=0.75),
+    quantile(fig_df$N,probs=0.75),
+    quantile(fig_df$N,probs=0.5),
+    quantile(fig_df$N,probs=0.5)
+                                          )),col=cbPalette[3],linewidth=2)+
+  xlab("Day post-infection")+ylab("Indiscriminate killing")+
+  theme_bw()+
+  theme(axis.title=element_text(size=15),
+        axis.text=element_blank(),
+        axis.ticks=element_blank(),
+        panel.grid=element_blank())
