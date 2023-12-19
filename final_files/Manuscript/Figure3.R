842bc425103e5dfdb24803591150790937498c36
Madeline <madelineapeters@gmail.com>
2023-12-19 16:36:49 -0500

    Update Figure3.R

--- final_files/Manuscript/Figure3.R
+++ final_files/Manuscript/Figure3.R
@@ -7,6 +7,7 @@ library(pomp)
 library(panelPomp)
 library(ggpubr)
 library(scales)
+library(aakmisc)
 
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
@@ -21,17 +22,18 @@ group_traj |>
   ggplot()+
   geom_path(aes(x=lagE,y=R,col=pABA),linewidth=2)+
   geom_text(aes(x=lagE,y=R,label=time),size=5)+
-  scale_x_continuous(labels = function(x) format(x, scientific = TRUE))+
-  scale_y_continuous(labels = function(x) format(x, scientific = TRUE))+
+  scale_x_continuous(labels=aakmisc::scinot)+
+  scale_y_continuous(labels=aakmisc::scinot)+
   scale_colour_manual(values=cbPalette[2:5])+
-  xlab("Erythrocytes (d-1)")+ylab("Reticulocyte supply (d)")+
+  xlab("Erythrocytes (t-1)")+ylab("RBC supply (t)")+
+  labs(colour="Parasite nutrient (pABA)")+
   theme_bw()+
   theme(
     axis.title=element_text(size=15),
     strip.text=element_blank(),
     axis.text=element_text(size=11),
     panel.grid=element_blank(),
-    legend.position=c(0.8,0.8),
+    legend.position=c(0.7,0.8),
     legend.title=element_text(size=15),
     legend.text=element_text(size=13),
     strip.background=element_blank(),
